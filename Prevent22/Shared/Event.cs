c560cfc021f6f19f7629e5eaee1fdf96e1b97e6c
Cory Sandlin <corybsa@gmail.com>
2021-01-25 13:15:13 -0500

    data models for events

--- Prevent22/Shared/Event.cs
+++ Prevent22/Shared/Event.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Prevent22.Shared
 {
 	public class Event
 	{
-		public Event() { }
-
-		public Event(string t, string d, DateTime s, DateTime e, bool i) {
-			Title = t;
-			Start = s;
-			End = e;
-			IsAllDay = i;
-		}
-
+		public int EventId { get; set; }
+		[Required(ErrorMessage = "Event title is required.")]
 		public string Title { get; set; }
 		public string Description { get; set; }
+		[Required(ErrorMessage = "Start date is required.")]
 		public DateTime Start { get; set; }
 		public DateTime End { get; set; }
 		public bool IsAllDay { get; set; }
+		public string RucrrenceRule { get; set; }
+		public int VolunteerCount { get; set; }
 	}
 }
--- Prevent22/Shared/Volunteer.cs
+++ Prevent22/Shared/Volunteer.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Prevent22.Shared
 {
-	public class User
+	public class Volunteer
 	{
-		public int UserId { get; set; }
-		public string Username { get; set; }
-		public string Password { get; set; }
-		public string Hash { get; set; }
-		public int RoleId { get; set; }
+		public int VolunteerId { get; set; }
+		public int EventId { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		public int UserId { get; set; }
+		public string Username { get; set; }
+		public int RoleId { get; set; }
 		public string Email { get; set; }
 		public string Country { get; set; }
 		public string State { get; set; }
-		public string City{ get; set; }
+		public string City { get; set; }
 		public string Address { get; set; }
 		public string Phone { get; set; }
 		public bool IsBanned { get; set; }
